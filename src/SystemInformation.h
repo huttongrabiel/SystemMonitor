696f895f91325b0f763cf1a756b55c366fdbd469
huttongrabiel <huttonthomas@icloud.com>
2022-03-13 19:33:20 -0700

    SystemInformation: Create Header File
    SystemInformation header file. Initial commit.

---
+++ src/SystemInformation.h
@@ -0,0 +1,17 @@
+//
+// Created by whuty on 3/13/22.
+//
+
+#ifndef SYSTEMMONITOR_SYSTEMINFORMATION_H
+#define SYSTEMMONITOR_SYSTEMINFORMATION_H
+
+#include <iostream>
+#include <string.h>
+
+class SystemInformation {
+public:
+    std::string cpu_temperature();
+    std::string uptime();
+};
+
+#endif //SYSTEMMONITOR_SYSTEMINFORMATION_H
