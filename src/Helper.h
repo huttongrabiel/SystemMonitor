f1c584e5261aff3e6a89412eafd33c1b3730ad2d
huttongrabiel <huttonthomas@icloud.com>
2022-03-13 19:42:42 -0700

    Helper: Add Helper.h File

---
+++ src/Helper.h
@@ -0,0 +1,15 @@
+//
+// Created by whuty on 3/13/22.
+//
+
+#ifndef SYSTEMMONITOR_HELPER_H
+#define SYSTEMMONITOR_HELPER_H
+
+#include <string.h>
+#include <iostream>
+class Helper {
+    int string_to_int(std::string);
+    std::string int_to_string(int);
+};
+
+#endif //SYSTEMMONITOR_HELPER_H
