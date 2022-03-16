26b7d00e4329bae6468dfeb1de24486e95164aa3
huttongrabiel <huttonthomas@icloud.com>
2022-03-15 19:17:02 -0700

    Helper: Implement int_to_string and string_to_int
    First two helper functions that I tend to use a lot. Probably
    not good code practice and I should really just write code that
    avoids conversions. But for now, this works.

--- src/Helper.cpp
+++ src/Helper.cpp
@@ -6,9 +6,29 @@
 #include <Helper.h>
 
 std::string Helper::int_to_string(int num) {
+    std::string reverse_retval;
 
+    while (num > 0) {
+        reverse_retval += (num % 10) + '0';
+        num /= 10;
+    }
+
+    std::string retval;
+    for (int i = reverse_retval.length(); i > 0; i++) {
+        retval += reverse_retval[i];
+    }
+
+    return retval;
 }
 
-int Helper::string_to_int(string) {
+int Helper::string_to_int(std::string str) {
+    int retval = 0;
 
+    int multiplier = 1;
+    for (int i = str.length()-1; i >= 0; i--) {
+        retval += multiplier * (str[i] - '0');
+        multiplier *= 10;
+    }
+
+    return retval;
 }
